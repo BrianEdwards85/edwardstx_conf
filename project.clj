86989022900916672425a3fad674116eeeb419f3
Brian Edwards <brian@edwardstx.us>
2017-03-04 04:11:22 +0300

    Preping to move to componenet

--- project.clj
+++ project.clj
@@ -6,19 +6,22 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/data.json "0.2.6"]
                  [org.clojure/tools.nrepl "0.2.12"]
+                 [clj-time "0.11.0"]
+
                  [yesql "0.5.3"]
                  [org.postgresql/postgresql "9.4.1208.jre7"]
                  [hikari-cp "1.7.5"]
+
+                 [yogthos/config "0.8"]
                  [com.rpl/specter "0.13.0"]
+                 [compojure "1.5.1"]
+                 [aleph "0.4.2-alpha12"]
+
                  [lock-key "1.4.1"]
                  [digest "1.4.5"]
-                 [yogthos/config "0.8"]
-                 [compojure "1.5.1"]
                  [buddy "1.2.0"]
                  [clj-crypto "1.0.2"
                   :exclusions [org.bouncycastle/bcprov-jdk15on bouncycastle/bcprov-jdk16]]
-                 [clj-time "0.11.0"]
-                 [aleph "0.4.2-alpha12"]
 
                  [org.clojure/tools.logging "0.3.1"]
                  [org.apache.logging.log4j/log4j-core "2.7"]
@@ -28,7 +31,7 @@
   :repositories [["spring.milestone" "https://repo.spring.io/libs-milestone"]]
 
   :main us.edwardstx.conf
-  :profiles {:uberjar {:aot :all}}
+  :profiles {:uberjar {:aot [us.edwardstx.conf.data.db]}}
 
   )
 

5bb72fa30d7f97eb673c9c6234c28ba59712e726
Brian Edwards <brian@edwardstx.us>
2017-03-15 00:43:33 +0300

    Cleanup

--- src/us/edwardstx/conf/logging.clj
+++ src/us/edwardstx/conf/logging.clj
@@ -1,75 +1,11 @@
 (ns us.edwardstx.conf.logging
-  (:import org.apache.logging.log4j.core.config.builder.api.ConfigurationBuilderFactory
-           org.apache.logging.log4j.Level
-           org.apache.logging.log4j.core.config.Configurator
-
+  (:import org.apache.logging.log4j.Level
            java.nio.charset.Charset
            org.apache.logging.log4j.LogManager
            org.apache.logging.log4j.core.layout.JsonLayout
            org.apache.logging.log4j.core.config.AppenderRef
            org.apache.logging.log4j.core.config.LoggerConfig
            org.springframework.amqp.rabbit.log4j2.AmqpAppender))
-
-(defn build-appender [builder {:keys [appender-name
-                                      addresses
-                                      user
-                                      password
-                                      virtualHost
-                                      exchange
-                                      exchangeType
-                                      declareExchange
-                                      durable
-                                      autoDelete
-                                      applicationId
-                                      routingKeyPattern
-                                      contentType
-                                      contentEncoding
-                                      generateId
-                                      deliveryMode
-                                      charset
-                                      senderPoolSize] :as  configuration}]
-  (let [appender-builder (.newAppender builder appender-name "RabbitMQ")]
-    (.addAttribute appender-builder "useSsl" "true" )
-    (.addAttribute appender-builder "addresses" addresses)
-    (.addAttribute appender-builder "user" user)
-    (.addAttribute appender-builder "password" password)
-    (.addAttribute appender-builder "virtualHost" virtualHost)
-    (.addAttribute appender-builder "exchange" exchange)
-    (.addAttribute appender-builder "exchangeType" exchangeType)
-    (.addAttribute appender-builder "declareExchange" declareExchange)
-    (.addAttribute appender-builder "durable" durable)
-    (.addAttribute appender-builder "autoDelete" autoDelete)
-    (.addAttribute appender-builder "applicationId" applicationId)
-    (.addAttribute appender-builder "routingKeyPattern" routingKeyPattern)
-    (.addAttribute appender-builder "contentType" contentType)
-    (.addAttribute appender-builder "contentEncoding" contentEncoding)
-    (.addAttribute appender-builder "generateId" generateId)
-    (.addAttribute appender-builder "deliveryMode" deliveryMode)
-    (.addAttribute appender-builder "charset" charset)
-    (.addAttribute appender-builder "senderPoolSize" senderPoolSize)
-    appender-builder))
-
-(defn build-layout [builder]
-  (let [layout-builder (.newLayout builder "JsonLayout")]
-    (.addAttribute layout-builder "complete" "true")
-    layout-builder))
-
-(defn reconfigure [{:keys [appender-name] :as configuration}]
-  (let [builder (ConfigurationBuilderFactory/newConfigurationBuilder)]
-    (.setStatusLevel builder Level/DEBUG)
-    (.setConfigurationName builder "RabbitMQLogger")
-    (let [appender-builder (build-appender builder configuration)
-;;          appender-ref (.newAppenderRef builder appender-name)
-          layout-builder (build-layout builder)]
-;;          root-loger (.newRootLogger builder Level/DEBUG)]
-      (.add appender-builder layout-builder)
-      (.add builder appender-builder)
-;;      (.add root-loger appender-ref)
-      (Configurator/initialize
-       (.build
-        (.add builder
-              (.add (.newRootLogger builder Level/DEBUG)
-                    (.newAppenderRef builder appender-name))))))))
 
 (def default-appender-settings {:useSsl true
                                 :declareExchange true
@@ -83,14 +19,6 @@
                                 :charset "UTF-8"
                                 :senderPoolSize 4
                                 :maxSenderRetries 30})
-
-
-
-(defn create-appender-ref [name]
-  (AppenderRef/createAppenderRef name nil nil))
-
-(defn create-appender-ref-array [name]
-  (to-array [(create-appender-ref name)]))
 
 (defn create-json-layout [configuration]
   (JsonLayout/createLayout configuration  ;;configuration
@@ -108,7 +36,8 @@
 
 (defn create-appender [configuration layout
                        {:keys [appender-name
-                               addresses
+                               host
+                               port
                                useSsl
                                user
                                password
@@ -132,9 +61,9 @@
                                  layout            ;;layout
                                  nil               ;;filter
                                  false             ;;ignoreExceptions
-                                 "localhost"       ;;host
-                                 5671              ;;port
-                                 nil   ;;addresses         ;;addresses
+                                 host              ;;host
+                                 port              ;;port
+                                 nil               ;;addresses
                                  user              ;;user
                                  password          ;;password
                                  virtualHost       ;;virtualHost
